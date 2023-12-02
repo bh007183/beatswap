13ab527c04621efa81279546d4731438592445bc
Ben Hopkins <bjhops17@gmail.com>
2023-12-02 15:01:13 -0800

    tried adding cdn access but struggled with that to

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState, setState} from 'react'
+// import DZ from "dzcdn"
 
 import axios from 'axios'
 
@@ -11,22 +12,53 @@ export default function Home() {
         secret:""
     }
 
-    async function CallDeezer(){
-        let appid = useState(state.appid)
-        let secret = useState(state.secret)
+    // async function CallDeezer(){
+    //   const DZ = window.DZ
+    //  let response = await DZ.init({
+    //     appId  : '651351',
+    //    channelUrl : 'http://localhost:3000/deezer.html'
+    //  })
+    //  console.log(response)
+    //  DZ.login(function(response) {
+
+    //     if (response.authResponse) {
+    //       console.log('Welcome!  Fetching your information.... ');
+    //       DZ.api('/user/me', function(response) {
+    //         console.log('Good to see you, ' + response.name + '.');
+    //       });
+    //     } else {
+    //       console.log('User cancelled login or did not fully authorize.');
+    //     }
+    //   }, {perms: 'basic_access,email'});
        
-    }
+    // }
+
+    
 
     useEffect(() => {
+      
+      CallDeezer()
+   
+      // DZ.login(function(response) {
+      //   if (response.authResponse) {
+      //     console.log('Welcome!  Fetching your information.... ');
+      //     DZ.api('/user/me', function(response) {
+      //       console.log('Good to see you, ' + response.name + '.');
+      //     });
+      //   } else {
+      //     console.log('User cancelled login or did not fully authorize.');
+      //   }
+      // }, {perms: 'basic_access,email'});
         
     //   setState(state =>{
     //      state.appid = process.env.REACT_APP_DEEZER_APPID,
     //      state.secret=process.env.REACT_APP_DEEZER_SECRET
     //   })
       console.log(state)
+    
       
 
-    }, [state])
+    }, [])
     
 
    
