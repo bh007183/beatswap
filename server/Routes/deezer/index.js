f7eef8d0581d4ec855875a0f7115fd89c722315f
Ben Hopkins <bjhops17@gmail.com>
2023-12-06 12:05:33 -0800

    added fs module

--- server/Routes/deezer/index.js
+++ server/Routes/deezer/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import axios from "axios";
 const router = express.Router();
+import * as fs from 'fs'
 
 //Deezer API Route To Initialize Deezer API Login
-router.get("/api", async (req, res) => {
+router.get("/api", async (req, res, next) => {
   let data = await axios.get(
     `https://connect.deezer.com/oauth/auth.php?app_id=651351&redirect_uri=http://localhost:3001/deezer&perms=basic_access,email,manage_library,delete_library`
   );
@@ -19,7 +20,7 @@ router.get("/", async (req, res) => {
   let accessTokenObj = await axios.get(
     `https://connect.deezer.com/oauth/access_token.php?app_id=${process.env.APPID}&secret=${process.env.SECRETKEY}&code=${res.req.query.code}&output=json`
   );
-  console.log(accessTokenObj.data);
+  
   if (!accessTokenObj.data.access_token) {
     // res.render("/deezererror");
   }
@@ -28,40 +29,39 @@ router.get("/", async (req, res) => {
     `https://api.deezer.com/user/me/playlists?access_token=${accessTokenObj.data.access_token}`
   );
 
-  if (playlistData.data.data.length === 0) {
+  if (!playlistData.data.data.length) {
     res.status(404).json({ message: "Unable to find any deezer playlists" });
    
   }
 
-  console.log(playlistData.data.data)
+  // console.log(playlistData.data.data)
   ///ACCESS HAS BEEN GRANTED NOW QUERRING ALL PLAYLISTS AND SONGS AND PUTTING THEM INTO VARIABLE PLAYLISTS
   let playlists = await Promise.all(playlistData.data.data.map((v, i) => {
-
-    console.log(v.link.replace('www', 'api') + "&limit=300")
-    // return axios.get(v.link.replace('www', 'api') + "&limit=300");
-
+     console.log(v.link.replace('www', 'api') + "&limit=300")
+     return axios.get(v.link.replace('www', 'api') + "&limit=300");
   }));
-//   console.log(playlists)
-// (async function(){
-//     for await (let val of playlists){
-//         console.log(val)
-//         // let allData = {
-//         //     title: v.title,
-//         //     length: songs.total,
-//         //     songs: songs.data.map((v, i) => {
-//         //       return {
-//         //         title: v.title,
-//         //         titleShort: v.title_short,
-//         //         artist: v.artist.name,
-//         //         album: v.album.title,
-//         //       };
-//         //     }),
-//         //   };
-//         //   return allData
-//       }
-// })
+
+  let combinedData = playlists.map((v,i) => {
+     return {
+            title: v.data.title,
+            nb_tracks: v.data.nb_tracks,
+            songs: v.data.tracks.data.map((v, i) => {
+              return {
+                title: v.title,
+                titleShort: v.title_short,
+                artist: v.artist.name,
+                album: v.album.title,
+              };
+            }),
+          };
+          
+  })
+
+   fs.writeFileSync("../../UserData/user.json", JSON.stringify(combinedData))
+
+ res.render("http://localhost:3000/deezer")
   
-//   console.log(playlists)
+
 });
 
 export default router;
